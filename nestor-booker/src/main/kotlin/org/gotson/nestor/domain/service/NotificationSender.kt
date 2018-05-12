4c4cd72850e4b681e3043fef5539bb2890b13faa
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2018-05-12 15:25:20 +0800

    application startup test

--- nestor-booker/src/main/kotlin/org/gotson/nestor/domain/service/NotificationSender.kt
+++ nestor-booker/src/main/kotlin/org/gotson/nestor/domain/service/NotificationSender.kt
@@ -12,17 +12,20 @@ import java.time.format.DateTimeFormatter
 @Service
 class NotificationSender @Autowired constructor(
     @Value("\${nestor.email.format.date:EEE d MMM yyyy}")
-        private val dateFormat: String,
+    private val dateFormat: String,
+
     @Value("\${nestor.email.format.time:HH:mm}")
-        private val timeFormat: String,
+    private val timeFormat: String,
+
     private val emailSender: EmailSender
 ) {
-    private val formatterDate = DateTimeFormatter.ofPattern(dateFormat)
-    private val formatterTime = DateTimeFormatter.ofPattern(timeFormat)
 
-    fun notifySuccessfulBooking(cl: PlannedClass, user: User) {
-        val subject = "[Nestor] Booked ${cl.type} at ${cl.dateTime.format(formatterTime)} on ${cl.dateTime.format(formatterDate)}"
-        val body = """Nestor booked a class for you!
+  private val formatterDate = DateTimeFormatter.ofPattern(dateFormat)
+  private val formatterTime = DateTimeFormatter.ofPattern(timeFormat)
+
+  fun notifySuccessfulBooking(cl: PlannedClass, user: User) {
+    val subject = "[Nestor] Booked ${cl.type} at ${cl.dateTime.format(formatterTime)} on ${cl.dateTime.format(formatterDate)}"
+    val body = """Nestor booked a class for you!
                     |
                     |Class: ${cl.type}
                     |Instructor: ${cl.instructor}
@@ -31,12 +34,12 @@ class NotificationSender @Autowired constructor(
                     |
                 """.trimMargin()
 
-        emailSender.sendEmail(subject, body, user.email)
-    }
+    emailSender.sendEmail(subject, body, user.email)
+  }
 
-    fun notifyWaitlistedBooking(cl: PlannedClass, user: User) {
-        val subject = "[Nestor] Waitlisted ${cl.type} at ${cl.dateTime.format(formatterTime)} on ${cl.dateTime.format(formatterDate)}"
-        val body = """Nestor booked a class for you on waitlist!
+  fun notifyWaitlistedBooking(cl: PlannedClass, user: User) {
+    val subject = "[Nestor] Waitlisted ${cl.type} at ${cl.dateTime.format(formatterTime)} on ${cl.dateTime.format(formatterDate)}"
+    val body = """Nestor booked a class for you on waitlist!
                     |
                     |Class: ${cl.type}
                     |Instructor: ${cl.instructor}
@@ -45,12 +48,12 @@ class NotificationSender @Autowired constructor(
                     |
                 """.trimMargin()
 
-        emailSender.sendEmail(subject, body, user.email)
-    }
+    emailSender.sendEmail(subject, body, user.email)
+  }
 
-    fun notifyBookingError(cl: PlannedClass, user: User) {
-        val subject = "[Nestor] An error occured while booking"
-        val body = """Nestor was trying to book a class for you, but an error occured :(
+  fun notifyBookingError(cl: PlannedClass, user: User) {
+    val subject = "[Nestor] An error occured while booking"
+    val body = """Nestor was trying to book a class for you, but an error occured :(
                     |
                     |Class: ${cl.type}
                     |Instructor: ${cl.instructor}
@@ -59,12 +62,12 @@ class NotificationSender @Autowired constructor(
                     |
                 """.trimMargin()
 
-        emailSender.sendEmail(subject, body, user.email)
-    }
+    emailSender.sendEmail(subject, body, user.email)
+  }
 
-    fun notifyNoMatchFound(cl: WishedClassDated) {
-        val subject = "[Nestor] No matching class found"
-        val body = """Nestor was trying to book a class for you, but no match could be found
+  fun notifyNoMatchFound(cl: WishedClassDated) {
+    val subject = "[Nestor] No matching class found"
+    val body = """Nestor was trying to book a class for you, but no match could be found
                     |
                     |Wished class
                     |Class: ${cl.type}
@@ -73,6 +76,6 @@ class NotificationSender @Autowired constructor(
                     |
                 """.trimMargin()
 
-        emailSender.sendEmail(subject, body, cl.user.email)
-    }
+    emailSender.sendEmail(subject, body, cl.user.email)
+  }
 }
