9c7e0373d92dbf1b7ced9249e527a85b545e71f6
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2019-04-02 17:40:18 +0800

    removed jackson-datatype-jsr310 as it is included via spring-boot-starter-web

--- nestor/build.gradle.kts
+++ nestor/build.gradle.kts
@@ -33,9 +33,7 @@ dependencies {
   implementation("io.springfox:springfox-swagger2:2.9.2")
   implementation("io.springfox:springfox-swagger-ui:2.9.2")
 
-  val jacksonVersion = "2.9.8"
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
 
   implementation("net.sf.biweekly:biweekly:0.6.3")
 
